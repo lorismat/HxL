a3c52f41d997eb8bcfbcba0c5ee41b3db0233da8
Loris M <loris.mattioni@gmail.com>
2023-02-22 16:40:55 +0100

    music frequency analyser test

--- components/TheCanvasMusic.vue
+++ components/TheCanvasMusic.vue
@@ -7,14 +7,25 @@
 <script setup>
 import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import * as Tone from 'tone';
+
+// tonejs 
+// all package
 
 let stats;
 let scene, renderer, camera, canvas;
 let mesh;
 
-let increment=0;
-
 const debug = false;
+
+const frequencyArraySize = useState('frequencyArraySize');
+const analyserDCMeter = useState('analyserDCMeter');
+const analyserMeter = useState('analyserMeter');
+const analyserFourier = useState('analyserFourier');
+
+let minArray = [0];
 
 function init() {
   scene = new THREE.Scene();
@@ -29,17 +40,79 @@ function init() {
   renderer = new THREE.WebGLRenderer({ antialias : true, canvas });
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize(window.innerWidth, window.innerHeight);
-
   renderer.setClearColor(0x123456);
 
-  camera.position.set(0,0,200);
+  camera.position.set(0,0,1000);
   camera.lookAt( scene.position );
+
+  // add controls
+  // const controls = new OrbitControls(camera, renderer.domElement);
+  // controls.maxDistance = 2000;
+  // controls.minDistance = 1000;
+
+  // number of lines depends on frequency
+  // check what we can get as information
 
   stats = new Stats();
   if (debug) document.body.appendChild( stats.dom );
 
-  const material = new THREE.MeshNormalMaterial();
-  const geometry = new THREE.SphereGeometry(10, 32, 32);
+  // to improve
+  const arrSize = 82;
+
+  // create shader material
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      time: { value: 0 },
+      resolution: { value: new THREE.Vector2() },
+      fArray: { value: new Float32Array(frequencyArraySize.value) }
+
+    },
+    vertexShader: `
+      varying vec2 vUv;
+      void main() {
+        vec3 pos = position;
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+      }
+    `,
+    fragmentShader: `
+      uniform float time;
+      uniform vec2 resolution;
+      uniform float fArray[${arrSize}];
+      
+
+      float plot(vec2 st, float pct){
+        return  smoothstep( pct-0.1, pct, st.y) -
+                smoothstep( pct, pct+0.1, st.y);
+      }
+
+
+      varying vec2 vUv;
+      void main() {
+
+        vec2 st = vUv;
+        vec2 store = vUv;
+
+        float size = float(${arrSize});
+        st.y = fract(st.y * size);
+
+        // float y = sin(st.x * 2. * 3.14 + 0.1 * fArray[ int(floor(store.y * size)) ] ) * 4. / 10. + 0.5;
+        // float y = sin(st.x * 2. * 3.14 * 100. + time * 2.) * 4. / 10. + 0.5;
+        // float y = sin(st.x * 2. * 3.14 * 10. * abs( fArray[ int(floor(store.y * size)) ]  ) + time * 2.) * 4. / 10. + 0.5;
+
+        float y = sin(st.x * 4. * 3.14 * 10. + time * 8.) * abs( fArray[ int(floor(store.y * size)) ] * 100.  ) * 0.00008 * 4. / 10. + 0.5;
+    
+        float pct = plot(st,y);
+        
+        vec3 col = vec3(1.);
+        col = (1.0-pct)*col+pct*vec3(0.071,0.204,0.337);
+
+        gl_FragColor = vec4(col, 1.0);
+      }
+    `,
+  });
+
+  const geometry = new THREE.SphereGeometry(500, 64, 64);
   mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
   
@@ -49,13 +122,46 @@ function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   stats.update();
-  if (renderer.info.render.frame == 180 && debug == true) console.log(renderer.info.render);
 
-  // fix with scrollama offset: scrollVal.value goes from 0.1 to 0.9
-  increment += 0.01;
+  // update shader material
+  mesh.material.uniforms.time.value = performance.now() / 1000;
+  mesh.material.uniforms.resolution.value.x = renderer.domElement.width;
+  mesh.material.uniforms.resolution.value.y = renderer.domElement.height;
 
-  mesh.position.x = 100 * Math.cos(increment);
-  mesh.position.y = 100 * Math.sin(increment);
+
+  if (analyserFourier.value != undefined) {
+
+    const fArray = analyserFourier.value.getValue();
+    minArray = [];
+
+    for (let i = 0; i < fArray.length; i += 100) {
+      minArray.push(fArray[i]);
+    }
+
+    console.log(minArray.length);
+
+    mesh.material.uniforms.fArray.value = minArray;
+
+    mesh.rotation.x += 0.001;
+    mesh.rotation.y += 0.001;
+    mesh.rotation.z += 0.001;
+
+    // mesh.scale.x += analyserDCMeter.value.getValue();
+    // mesh.scale.y = Math.abs(analyserMeter.value.getValue());
+    // mesh.scale.z = Math.abs(analyserFourier.value.getValue()[0]);
+  }
+
+  // if frame 200
+  if (renderer.info.render.frame == 200 && debug == true) {
+    console.log(mesh.material.uniforms.fArray.value);
+  }
+
+  if (renderer.info.render.frame == 400 && debug == true) {
+    console.log(mesh.material.uniforms.fArray.value);
+  }
+
+
+  
 }
 
 function onWindowResize() {
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,101 @@
 <template>
   <div>
-    <ScrollamaImplem />
+    <!--
+      <ScrollamaImplem />
+    -->
+    
     <TheCanvasMusic />
+
+    <button @click="triggerSound">
+      {{ playerText }} 
+    </button>
+
   </div>
 </template>
 
 <script setup>
+import * as Tone from 'tone';
+const playerText = ref('play');
+const initTone = ref(0);
 
-const scrollVal = useState('scrollVal', () => 0); // https://nuxt.com/docs/getting-started/state-management
+let player;
+
+// set up the array size of frequency which will be the number of lines
+// pow of 2
+const frequencyArraySize = useState('frequencyArraySize', () => Math.pow(2,13) );
+
+
+const analyserDCMeter = useState('analyserDCMeter', () => {} );
+const analyserMeter = useState('analyserMeter', () => {} );
+const analyserFourier = useState('analyserFourier', () => {} ); 
+
+function triggerSound() {
+
+  if (initTone.value === 0) {
+    // putting file into buffer
+    const audioFile = "sounds/5.mp3";
+    player = new Tone.Player(audioFile).toDestination();
+
+    const tempDCMeter = new Tone.DCMeter();
+    const tempMeter = new Tone.Meter();
+    const tempFourier = new Tone.Analyser("fft", frequencyArraySize.value);
+    tempFourier.smoothing = 0.00000001;
+    
+    player.connect(tempDCMeter);
+    player.connect(tempMeter);
+    player.connect(tempFourier);
+
+    analyserDCMeter.value = tempDCMeter;
+    analyserMeter.value = tempMeter;
+
+    // waveform
+    // The values in the array are normalized to a range between -1 and 1, where -1 represents the minimum amplitude (i.e. silence), 
+    // and 1 represents the maximum amplitude (i.e. the loudest part of the audio signal).
+
+    // fft
+
+    // In this example, the Analyser is configured to use the "waveform" type, 
+    // which means it will provide an array of amplitude values representing the waveform of the audio signal.
+
+    // The FFT (Fast Fourier Transform) is a mathematical algorithm used to convert a time-domain signal 
+    // (like an audio waveform) into its frequency-domain representation (i.e. a spectrum). 
+    analyserFourier.value = tempFourier;
+
+    player.loop=true;
+    player.autostart=true;
+
+    playerText.value = 'stop';
+    initTone.value = 1;
+
+    
+
+  } else {
+    if (playerText.value === 'play') {
+      player.start();
+      playerText.value = 'stop';
+    } else {
+      player.stop();
+      playerText.value = 'play';
+    }
+  }
+
+}
 
 </script>
+
+<style scoped>
+button {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  padding: 6px 12px;
+  font-family: monospace;
+  margin:30px;
+  background-color: #333;
+  color:#fff;
+  border: 2px solid #fff;
+  cursor: pointer;
+  border-radius: 6px;
+}
+</style>
